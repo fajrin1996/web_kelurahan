a051beca6b8bd830b574a3c3ae083275fad35eba
fajrin1996 <77439859+fajrin1996@users.noreply.github.com>
2022-09-01 17:20:07 +0900

    Update __init__.py

--- affairs/__init__.py
+++ affairs/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, redirect, url_for
 #form input
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-from affairs.admin.routes import admingam
+
 
 #Database import
 import datetime
@@ -13,7 +13,7 @@ from flask_ckeditor import CKEditor
 app = Flask("__name__", template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = "affairs"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///kelurahan.db'
-app.register_blueprint(admingam)
+
 
 ckeditor = CKEditor(app)
 app.config['CKEDITOR_HEIGHT'] = 400
@@ -21,7 +21,9 @@ db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 
+from affairs.admin.routes import admingam
 
+app.register_blueprint(admingam)
 
 
 
