a82a45d4078dc84a5ee6aa0c50f1d36048d3745e
fajrin1996 <77439859+fajrin1996@users.noreply.github.com>
2022-09-10 00:18:09 +0900

    Update __init__.py

--- affairs/__init__.py
+++ affairs/__init__.py
@@ -12,7 +12,7 @@ from flask_ckeditor import CKEditor
  
 app = Flask("__name__", template_folder='affairs/templates', static_folder='affairs/static')
 app.config['SECRET_KEY'] = "affairs"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///kelurahan.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://fvrixdnkelwmhi:caa04fc6e0b16716f10eb62d1e4a3564e9aa60e880a9a05bf10561ef30ab8921@ec2-54-159-175-38.compute-1.amazonaws.com:5432/db7slibuqricdq'
 
 ckeditor = CKEditor(app)
 app.config['CKEDITOR_HEIGHT'] = 400
